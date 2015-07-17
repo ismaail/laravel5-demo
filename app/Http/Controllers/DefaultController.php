a6f55d7aa8a38a54224790892701324721829d65
E.G. ismaail <contact@ismaail.com>
2015-07-17 19:58:36 +0000

    Add a function to send email

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Mail\Message;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Mail;
+use Config;
 
 /**
  * Class DefaultController
@@ -19,5 +21,21 @@ class DefaultController extends Controller
     public function index()
     {
         return view('default/index');
+    }
+
+    /**
+     * @param string $to
+     * @param string $subject
+     * @param array $body
+     */
+    private function sendEmail($to, $subject, $body)
+    {
+        Mail::send('emails/blank', ['body' => $body], function (Message $message) use ($to, $subject) {
+            $mailConfig = Config::get('mail');
+
+            $message->from($mailConfig['from']['address'], $mailConfig['from']['name']);
+            $message->to($to)
+                    ->subject($subject);
+        });
     }
 }
---
+++ resources/views/emails/blank.blade.php
@@ -0,0 +1,3 @@
+<h1>Email template</h1>
+
+<p>{{ $body }}</p>

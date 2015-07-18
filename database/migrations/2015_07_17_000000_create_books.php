ab26aaee0e342fcaac497676a973485f94184a12
E.G. ismaail <contact@ismaail.com>
2015-07-18 18:23:50 +0100

    Add books table migration

---
+++ database/migrations/2015_07_17_000000_create_books.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migration
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 150);
+            $table->string('slug', 150);
+            $table->text('description');
+            $table->integer('pages', false, true);
+        });
+    }
+
+    /**
+     * Rollback the migration
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}

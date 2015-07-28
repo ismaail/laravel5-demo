c98d05098e7d86e6d316c453267338a96c0226e2
E.G. ismaail <contact@ismaail.com>
2015-07-28 22:59:13 +0100

    layout view: show error messages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,16 @@
         </div>
     </header>
     <div class="container">
+        @if (count($errors))
+            <div class="alert alert-danger">
+                <strong>Error:</strong>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         @yield("content")
     </div>
 </body>

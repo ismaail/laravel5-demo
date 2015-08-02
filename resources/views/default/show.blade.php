bf98e80be82e580ebee88fa2d045e02a8d37a2d3
E.G. ismaail <contact@ismaail.com>
2015-08-02 20:08:31 +0100

    book show view: add link to edit book

--- resources/views/default/show.blade.php
+++ resources/views/default/show.blade.php
@@ -3,11 +3,16 @@
 @section("content")
     <article>
         <h1>{{ $book->title }}</h1>
-        <div>
+        <p>
             Number of pages: {{ $book->pages }}
-        </div>
-        <div>
+        </p>
+        <p>
             {{ $book->description }}
+        </p>
+        <div class="col-sm-4 col-md-2">
+            <a href="/books/{{ $book->slug }}/edit" class="button-link">
+                <button class="input-sm form-control">Edit</button>
+            </a>
         </div>
     </article>
 @endsection

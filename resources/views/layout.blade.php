8ebbe5e74aa77e7ad2edb835823e56eddec82981
E.G. ismaail <contact@ismaail.com>
2015-08-02 22:55:21 +0100

    layout view: display alert success message

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,6 +21,11 @@
         </div>
     </header>
     <div class="container">
+        @if (Session::has('success_message'))
+            <div class="alert alert-success">
+                {{ Session::get('success_message') }}
+            </div>
+        @endif
         @if (count($errors))
             <div class="alert alert-danger">
                 <strong>Error:</strong>

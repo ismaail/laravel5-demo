9f8f33b438bc4334db25ece26e4a98a9197f30b4
E.G. ismaail <contact@ismaail.com>
2015-08-15 21:43:06 +0100

    Remove data methods from App\Book

--- app/Book.php
+++ app/Book.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Cviebrock\EloquentSluggable\SluggableInterface;
 use Cviebrock\EloquentSluggable\SluggableTrait;
 
@@ -47,53 +46,4 @@ class Book extends Model implements SluggableInterface
         'build_from' => 'title',
         'save_to'    => 'slug',
     ];
-
-    /**
-     * Find All books
-     *
-     * @param int $limit
-     *
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
-     */
-    public static function findAll($limit)
-    {
-        return \DB::table('book')->paginate($limit);
-    }
-
-    /**
-     * Find Book by slug
-     *
-     * @param string $slug
-     *
-     * @return Book
-     */
-    public static function findBySlug($slug)
-    {
-        $book = self::where('slug', $slug)->first();
-
-        if (is_null($book)) {
-            throw new ModelNotFoundException("Book not found");
-        }
-
-        return $book;
-    }
-
-    /**
-     * Find book by slug or fail to 404 page
-     *
-     * @param string $slug
-     *
-     * @return Book
-     */
-    public static function findBySlugOrFail($slug)
-    {
-        try {
-            $book = self::findBySlug($slug);
-
-            return $book;
-
-        } catch (ModelNotFoundException $e) {
-            \App::abort(404, $e->getMessage());
-        }
-    }
 }
